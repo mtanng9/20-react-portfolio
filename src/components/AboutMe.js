c07f6e9ed63346acd5cb5b57e343037ace5a5dbd
My Tang <my@Mys-MacBook-Air.local>
2023-05-01 01:13:31 +0700

    created an about me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,16 +1,25 @@
 function AboutMe() {
     return (
-        <div className="description text-center">
-            <p>
-                As a Full Stack Developer with a background in Chemistry and Bioengineering, 
-                I bring a critical problem-solving approach, a drive for experimentation and learning, 
-                and an aptitude for building projects from the ground up. 
-                I have hands-on experience working with programming languages like React, JavaScript, 
-                and back-end technologies such as Node.js and SQL, which allows me to tackle challenging projects 
-                with creativity and resourcefulness. 
-                If you're interested in learning more about my experience and approach to software engineering, 
-                please don't hesitate to get in touch.
-            </p>
+
+        <div id="about-me-container">
+            <div className="description text-center">
+                <p>
+                    As a Full Stack Developer with a background in Chemistry and Bioengineering, 
+                    I bring a critical problem-solving approach, a drive for experimentation and learning, 
+                    and an aptitude for building projects from the ground up. 
+                    I have hands-on experience working with programming languages like React, JavaScript, 
+                    and back-end technologies such as Node.js and SQL, which allows me to tackle challenging projects 
+                    with creativity and resourcefulness. 
+                    If you're interested in learning more about my experience and approach to software engineering, 
+                    please don't hesitate to get in touch.
+                </p>
+            </div>
+            <div className="avatar">
+                <img 
+                    src="https://media.licdn.com/dms/image/D4E03AQGaRO0INzOIEQ/profile-displayphoto-shrink_400_400/0/1680286196950?e=1688601600&v=beta&t=4g4xvXUo8MhZ-W-QrLL4SAsbZfT1naZtu_bmWk_3_oU"
+                    alt="Developer Profile" 
+                />
+            </div>
         </div>
 
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 function Footer() {
     return (
-        <div>
-            <p>hi</p>
+        <div className="footer" >
             <a href="https://github.com/mtanng9" className="btn btn-just-icon btn-link btn-github"><i className="fa fa-github"></i></a>
             <a href="https://www.linkedin.com/in/mymtang/" className="btn btn-just-icon btn-link btn-linkedin"><i className="fa fa-linkedin"></i></a>
             <a href="https://stackoverflow.com/" className="btn btn-just-icon btn-link btn-stack-overflow"><i className="fa fa-stack-overflow"></i></a>
